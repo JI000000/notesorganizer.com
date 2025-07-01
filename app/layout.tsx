a61358f3397598530ab4528041d06086d38d6a4e
JI000000 <jipengfei0806@gmail.com>
2025-07-02 01:53:42 +0800

    feat: integrate google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import GoogleAnalytics from "@/components/layout/GoogleAnalytics";
 import "./../styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -10,6 +11,8 @@ export const metadata: Metadata = {
   description: "The definitive hub for frameworks, guides, and AI-powered tools to transform your scattered notes into a structured, interconnected knowledge base.",
 };
 
+const GA_MEASUREMENT_ID = "G-T3JM54B7WM";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -18,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body className={`${inter.className} bg-brand-dark text-gray-200`}>
+        <GoogleAnalytics gaId={GA_MEASUREMENT_ID} />
         {children}
         <Analytics />
       </body>
